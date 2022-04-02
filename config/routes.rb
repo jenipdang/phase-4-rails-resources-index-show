e886736cac36361d0c9768baf2934b25d40174f9
Jeni Dang <jenipdang@yahoo.com>
2022-04-01 20:46:03 -0700

    commit tree 4e73195958512e51c5efaa616383fff34f360ebe

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/birds', to: 'brids#index'
+  # get '/birds/:id', to: 'brids#show'
+  resources :birds, only: [:index, :show]
 end
